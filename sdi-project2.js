757c8e71dccf421fda2186cadd65cb77c505480b
steve1121 <steve1121345@gmail.com>
2014-09-11 11:51:45 -0400

    this is it so far

--- sdi-project2.js
+++ sdi-project2.js
@@ -3,5 +3,9 @@
 //Author: Steven Beeson
 
 
+var me = "Steve"
+var avgNumOfDel = 15 //average number of deliveries
+var takeHomeGoal = 100 + "$" // goal for cash in pocket
+var avgMilesPerDel = 5 + " miles" //average miles per deliveries
+var avgTipPerDel = 2 + "$" // average tip per deliveries
 
-var 

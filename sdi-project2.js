932443b2a9d6d708e5cb0325ae677431ef225b49
steve1121 <steve1121345@gmail.com>
2014-09-11 19:40:37 -0400

    starting to get a creative flow going

--- sdi-project2.js
+++ sdi-project2.js
@@ -3,9 +3,20 @@
 //Author: Steven Beeson
 
 
-var me = "Steve"
-var avgNumOfDel = 15 //average number of deliveries
-var takeHomeGoal = 100 + "$" // goal for cash in pocket
-var avgMilesPerDel = 5 + " miles" //average miles per deliveries
-var avgTipPerDel = 2 + "$" // average tip per deliveries
+var  k = "Knight Gildar";
+var p = "Princess Pop";
+var d = "Evil Dragon";
+var l = "long sword";
+var lStrength = 55
+var sc = "fire magic scale"
+var kDef = 67
+var scStrength = 58
+var kDef = 89
 
+console.log("There once was a good dragon name " + d + ".");
+console.log("His greatest enemy was the dreaded " + k + "!");
+console.log(k + " was always beating poor " + d + " up and stealing his poor " + p + "!");
+console.log(p + " was " + d + "'s best friend and " + k +" always kidnapped her and made her feed him every night.")
+console.log("Whereas " + d + " was always letting her do as she pleased as long as he could admire her.")
+console.log("After so long " + d + " decided enough was enough, he was fighting back!");
+console.log("He needed to prepare, big time.");

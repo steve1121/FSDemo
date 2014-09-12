1e0157a1cf530377498c509a5c9d85a3fb5888b9
steve1121 <steve1121345@gmail.com>
2014-09-11 23:09:43 -0400

    all I could get

--- sdi-project2.js
+++ sdi-project2.js
@@ -6,17 +6,39 @@
 var  k = "Knight Gildar";
 var p = "Princess Pop";
 var d = "Evil Dragon";
-var l = "long sword";
-var lStrength = 55
-var sc = "fire magic scale"
-var kDef = 67
-var scStrength = 58
-var kDef = 89
 
-console.log("There once was a good dragon name " + d + ".");
+console.log("There once was a good dragon named " + d + ".");
 console.log("His greatest enemy was the dreaded " + k + "!");
 console.log(k + " was always beating poor " + d + " up and stealing his poor " + p + "!");
 console.log(p + " was " + d + "'s best friend and " + k +" always kidnapped her and made her feed him every night.")
-console.log("Whereas " + d + " was always letting her do as she pleased as long as he could admire her.")
+console.log("Whereas " + d + " was always letting her do as she pleased as long as he could admire " + p + ".")
 console.log("After so long " + d + " decided enough was enough, he was fighting back!");
 console.log("He needed to prepare, big time.");
+
+var sc = prompt ("What weapon should " + d + " weild?")
+var scStrength = prompt ("What strength should the " + sc + " possess?")
+var dDef = prompt ("How much defense should " + d + " possess?")
+var ls = prompt ("What weapon should " + k + " weild?")
+var lStrength = prompt ("What strength should the " + ls + " possess?")
+var kDef = prompt ("How much defense should " + k + " possess?")
+
+console.log(d + " equipped the " + sc + " with a strength of " + scStrength + ".")
+console.log('He\'s already had a defense of ' + dDef + ".")
+console.log(k + " equipped the " + ls + " with a strength of " + lStrength + ".")
+console.log('He\'s already had a defense of ' + kDef + ".")
+
+for (lStrength; lStrength <= dDef; dDef - lStrength / 2) {
+	for (lStrength; lStrength >= dDef; dDef - lStrength) {
+		console.log (k + " attacks " + d + '.')
+		console.log (d + '\'s' + " defense is now at " + dDef + '.')
+	};
+};
+for (scStrength; scStrength <= kDef; kDef - scStrength / 2) {
+	for(scStrength; scStrength >= kDef; kDef - scStrength) {
+		console.log (d + ' attacks ' + k + '.')
+		console.log (k + '\'s defense is now at ' + kDef + '.')
+	};
+}; 
+// could not figure it out after this point with the little ammount of time I had personally
+
+// I think I should have wrapped it in a function just not sure how
